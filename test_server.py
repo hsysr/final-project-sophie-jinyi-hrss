b89fbaf95725b8d963e42797e1bf9780b599efe4
hsysr <xiejy.david2014@gmail.com>
2022-04-05 18:38:38 -0400

    add more testcases to unittests

--- test_server.py
+++ test_server.py
@@ -5,16 +5,26 @@ from server import init_server
 init_server()
 
 
-def test_add_patient():
+@pytest.mark.parametrize("info_for_test", [
+    {'MRN': 114520,
+     'name': 'Smith.J',
+     'medical_image': 'some medical_image',
+     'ECG_image': 'some ECG_image',
+     'heart_rate': 72},
+    {'MRN': 114520,
+     'name': 'Smith.J',
+     'medical_image': '',
+     'ECG_image': 'some ECG_image',
+     'heart_rate': 72},
+    {'MRN': 114520,
+     'name': 'Smith.J',
+     'medical_image': 'some medical_image',
+     'ECG_image': '',
+     'heart_rate': -1},
+])
+def test_add_patient(info_for_test):
     from database import Patient
     from server import add_patient
-    info_for_test = {
-        'MRN': 114520,
-        'name': 'Smith.J',
-        'medical_image': '',
-        'ECG_image': '',
-        'heart_rate': -1
-    }
     answer = add_patient(info_for_test, "2022-04-05 16:09:26")
     Patient.objects.raw({"_id": info_for_test['MRN']}).first().delete()
     assert answer == info_for_test['MRN']
@@ -36,7 +46,57 @@ def test_has_patient():
     Patient.objects.raw({"_id": test_patient.MRN}).first().delete()
 
 
-def test_update_patient():
+@pytest.mark.parametrize("test_update_info, expt_ans", [
+    [{'MRN': 114523,
+      'name': 'Smith.F',
+      'medical_image': 'some medical_image',
+      'ECG_image': 'some ECG_image',
+      'heart_rate': 72},
+     {'MRN': 114523,
+      'name': 'Smith.F',
+      'medical_image': ['some medical_image'],
+      'medical_timestamp': ["2022-04-05 16:09:26"],
+      'ECG_image': ['some ECG_image'],
+      'heart_rate': [72],
+      'ECG_timestamp': ["2022-04-05 16:09:26"]}],
+    [{'MRN': 114523,
+      'name': '',
+      'medical_image': 'some medical_image',
+      'ECG_image': 'some ECG_image',
+      'heart_rate': 72},
+     {'MRN': 114523,
+      'name': 'Smith.J',
+      'medical_image': ['some medical_image'],
+      'medical_timestamp': ["2022-04-05 16:09:26"],
+      'ECG_image': ['some ECG_image'],
+      'heart_rate': [72],
+      'ECG_timestamp': ["2022-04-05 16:09:26"]}],
+    [{'MRN': 114523,
+      'name': 'Smith.F',
+      'medical_image': '',
+      'ECG_image': 'some ECG_image',
+      'heart_rate': 72},
+     {'MRN': 114523,
+      'name': 'Smith.F',
+      'medical_image': [],
+      'medical_timestamp': [],
+      'ECG_image': ['some ECG_image'],
+      'heart_rate': [72],
+      'ECG_timestamp': ["2022-04-05 16:09:26"]}],
+    [{'MRN': 114523,
+      'name': 'Smith.F',
+      'medical_image': 'some medical_image',
+      'ECG_image': '',
+      'heart_rate': -1},
+     {'MRN': 114523,
+      'name': 'Smith.F',
+      'medical_image': ['some medical_image'],
+      'medical_timestamp': ["2022-04-05 16:09:26"],
+      'ECG_image': [],
+      'heart_rate': [],
+      'ECG_timestamp': []}]
+])
+def test_update_patient(test_update_info, expt_ans):
     from database import Patient
     from server import update_patient
     test_patient = Patient(MRN=114523,
@@ -47,22 +107,15 @@ def test_update_patient():
                            ECG_image=[],
                            ECG_timestamp=[])
     test_patient.save()
-    test_update_info = {
-        'MRN': 114523,
-        'name': 'Smith.F',
-        'medical_image': 'some base64 string',
-        'ECG_image': 'some base64 string',
-        'heart_rate': 72
-    }
     test_timestamp = "2022-04-05 16:09:26"
     answer = update_patient(test_update_info, test_timestamp)
-    assert answer.MRN == test_update_info['MRN']
-    assert answer.name == test_update_info['name']
-    assert answer.medical_image == [test_update_info['medical_image']]
-    assert answer.medical_timestamp == [test_timestamp]
-    assert answer.heart_rate == [test_update_info['heart_rate']]
-    assert answer.ECG_image == [test_update_info['ECG_image']]
-    assert answer.ECG_timestamp == [test_timestamp]
+    assert answer.MRN == expt_ans['MRN']
+    assert answer.name == expt_ans['name']
+    assert answer.medical_image == expt_ans['medical_image']
+    assert answer.medical_timestamp == expt_ans['medical_timestamp']
+    assert answer.heart_rate == expt_ans['heart_rate']
+    assert answer.ECG_image == expt_ans['ECG_image']
+    assert answer.ECG_timestamp == expt_ans['ECG_timestamp']
     Patient.objects.raw({"_id": test_patient.MRN}).first().delete()
 
 
