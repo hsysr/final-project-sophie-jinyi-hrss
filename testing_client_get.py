ce7f539a94c091caa8cbc34eea4599d2e240e026
Sooophy <eggtart11@163.com>
2022-04-10 00:49:08 -0400

    add testing client to tets the retreive record route

--- testing_client_get.py
+++ testing_client_get.py
@@ -8,3 +8,7 @@ server = "http://127.0.0.1:5000"
 r = requests.get(server + "/api/station/mrnlist")
 print(r.status_code)
 print(r.text)
+
+r = requests.get(server + "/api/station/5")
+print(r.status_code)
+print(r.text)
